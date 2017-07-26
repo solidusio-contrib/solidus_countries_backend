fccc93a019e6d32b9d32f36130303e79a1696852
Alberto Vena <kennyadsl@gmail.com>
2017-07-26 23:11:22 +0200

    Move countries/states js from core

--- app/assets/javascripts/spree/backend/solidus_countries_backend.js
+++ app/assets/javascripts/spree/backend/solidus_countries_backend.js
@@ -1,2 +1,17 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
+
+$(document).ready(function(){
+  $(".country-select-observer").on('change', function() {
+    var target = $(this).data("update");
+    $(target).hide();
+    Spree.ajax({ dataType: 'html',
+      url: $(this).data("base-url")+encodeURIComponent($(this).val()),
+      type: 'get',
+      success: function(data){
+        $(target).html(data);
+        $(target).show();
+      }
+    });
+  });
+});
